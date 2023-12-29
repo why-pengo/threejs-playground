4fd562564892de1064032002be0e3a1e9741a14e
Jon Morgan <morgan.jon@gmail.com>
2023-12-29 17:07:38 -0500

    Base playground

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import "./style.scss";
 import typescriptLogo from "./typescript.svg";
+import threejsLogo from "./threejs.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.ts";
+import { setupTest1 } from "./test1.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -11,14 +13,21 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <a href="https://threejs.org/" target="_blank">
+      <img src="${threejsLogo}" class="logo vanilla" alt="Three.js logo" />
+    </a>
+    <h1>Vite + TypeScript + Three.js</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
+    <div class="card">
+      <button id="test1" type="button">Test1</button>
+    </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the Vite, TypeScript and Threejs logos to learn more
     </p>
   </div>
 `;
 
 setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+setupTest1(document.querySelector<HTMLButtonElement>("#test1")!);
